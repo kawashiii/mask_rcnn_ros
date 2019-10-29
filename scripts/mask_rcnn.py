80b6b4a57e113b31b7938b5ca3840c35fcbed063
kawashiii <turquoise12-1@hotmail.co.jp>
2019-10-29 17:06:23 +0900

    fix a little

--- scripts/mask_rcnn.py
+++ scripts/mask_rcnn.py
@@ -18,6 +18,7 @@ from mask_rcnn_ros.srv import Detect, DetectResponse
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import lab
 import cv2
+from math import atan2, cos, sin, sqrt, pi
 from cv_bridge import CvBridge
 
 from mrcnn import utils
@@ -130,7 +131,7 @@ class MaskRCNNNode(object):
 
     def run3(self):
         self._result_pub = rospy.Publisher('~result', Result, queue_size=1)
-        self._result_lab_pub = rospy.Publisher('~result_detection', Result_lab, queue_size=1)
+        self._result_lab_pub = rospy.Publisher('~result_lab', Result_lab, queue_size=1)
         self.vis_pub = rospy.Publisher('~visualization', Image, queue_size=1)
 
         self.detect_srv = rospy.Service('mask_rcnn/detect_objects', Detect, self._detect_objects)
@@ -184,9 +185,45 @@ class MaskRCNNNode(object):
         masks = result['masks']
         result_msg.count = len(scores)
         ids = []
-        for i in len(result['scores']):
-            ids.append(i)
-            
+
+    def drawAxis(self, img, p_, q_, colour, scale):
+        p = list(p_)
+        q = list(q_)
+        
+        angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+        hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+        # Here we lengthen the arrow by a factor of scale
+        q[0] = p[0] - scale * hypotenuse * cos(angle)
+        q[1] = p[1] - scale * hypotenuse * sin(angle)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+        # create the arrow hooks
+        p[0] = q[0] + 9 * cos(angle + pi / 4)
+        p[1] = q[1] + 9 * sin(angle + pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+        p[0] = q[0] + 9 * cos(angle - pi / 4)
+        p[1] = q[1] + 9 * sin(angle - pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+
+    def getOrientation(self, pts, img):    
+        sz = len(pts)
+        data_pts = np.empty((sz, 2), dtype=np.float64)
+        for i in range(data_pts.shape[0]):
+            data_pts[i,0] = pts[i,0,0]
+            data_pts[i,1] = pts[i,0,1]
+        # Perform PCA analysis
+        mean = np.empty((0))
+        mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+        # Store the center of the object
+        cntr = (int(mean[0,0]), int(mean[0,1]))
+
+        cv2.circle(img, cntr, 3, (255, 0, 255), 2)
+        p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+        p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+        drawAxis(img, cntr, p1, (0, 255, 0), 1)
+        drawAxis(img, cntr, p2, (255, 255, 0), 5)
+        angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+        
+        return angle               
 
 
     def _build_result_msg(self, msg, result):
@@ -227,6 +264,7 @@ class MaskRCNNNode(object):
         fig = Figure()
         canvas = FigureCanvasAgg(fig)
         axes = fig.gca()
+        masks = result['masks']
         visualize.display_instances(image, result['rois'], result['masks'],
                                     result['class_ids'], CLASS_NAMES,
                                     result['scores'], ax=axes)
@@ -236,6 +274,17 @@ class MaskRCNNNode(object):
 
         _, _, w, h = fig.bbox.bounds
         result = result.reshape((int(h), int(w), 3))
+
+        for i in range(masks.shape[-1]):
+            ret, thresh = cv2.threshold(mask[:,:,i], 0.5, 1.0, cv2.THRESH_BINARY)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for i, c in enumerate(contours):
+                area = cv2.contourArea(c)
+                if area < 1e2 or 1e5 < area:
+                    continue
+                cv2.drawContours(result, contours, i, (0, 0, 255), 2)
+                getOrientation(c, result)
+
         return result
 
     def _image_callback(self, msg):
