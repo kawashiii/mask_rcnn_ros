b8e4afc13e72c3d5086c780d94fb6fd6c3ad9359
kawashiii <turquoise12-1@hotmail.co.jp>
2019-11-22 16:47:56 +0900

    fix calibration setting

--- scripts/mask_rcnn.py
+++ scripts/mask_rcnn.py
@@ -8,7 +8,6 @@ import numpy as np
 
 import rospy
 import roslib.packages
-#from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import RegionOfInterest
 from geometry_msgs.msg import Vector3
@@ -19,7 +18,6 @@ from std_msgs.msg import UInt8MultiArray
 from phoxi_camera.srv import *
 from mask_rcnn_ros.msg import MaskRCNNMsg
 from mask_rcnn_ros.srv import MaskRCNNSrv, MaskRCNNSrvResponse
-#from cv_bridge import CvBridge
 
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import lab
@@ -32,10 +30,9 @@ from mrcnn import model as modellib
 from mrcnn import visualize
 
 ROOT_DIR = os.path.abspath(roslib.packages.get_pkg_dir('mask_rcnn_ros'))
-MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_lab.h5")
+MODEL = os.path.join(ROOT_DIR, "mask_rcnn_lab.h5")
+CAMERA_INTRINSIC = os.path.join(ROOT_DIR, "config/realsense_intrinsic.xml")
 TEST_IMG = os.path.join(ROOT_DIR, "test.png")
-
-CLASS_NAMES = lab.class_names
 
 class InferenceConfig(lab.LabConfig):
     # Set batch size to 1 since we'll be running inference on
@@ -45,105 +42,79 @@ class InferenceConfig(lab.LabConfig):
 
 class MaskRCNNNode(object):
     def __init__(self):
-        self._cv_bridge = CvBridge()
+        self.cv_bridge = CvBridge()
 
         config = InferenceConfig()
         config.display()
 
         # Create model object in inference mode.
-        self._model = modellib.MaskRCNN(mode="inference", model_dir="", config=config)
-        self._model.load_weights(MODEL_PATH, by_name=True)
-        self._model.keras_model._make_predict_function()
+        self.model = modellib.MaskRCNN(mode="inference", model_dir="", config=config)
+        self.model.load_weights(MODEL, by_name=True)
+        self.model.keras_model._make_predict_function()
 
-        self._last_msg = None
-        self._msg_lock = threading.Lock()
+        # Set calibration matrix
+        fs = cv2.FileStorage(CAMERA_INTRINSIC, cv2.FILE_STORAGE_READ)
+        self.camera_matrix = fs.getNode("camera_matrix").mat()
+        self.dist_coeffs = fs.getNode("distortion_coefficients").mat()
+        self.set_coordinate_trasformation()
 
-        # self._class_names = rospy.get_param('~class_names', CLASS_NAMES)
-        # self._visualization = rospy.get_param('~visualization', True)
-        # self._publish_rate = rospy.get_param('~publish_rate', 100)
-        self._class_names = CLASS_NAMES
-        self._visualization = True
-        self._publish_rate = 100
-        self._class_colors = visualize.random_colors(len(CLASS_NAMES))
+        self.class_names = lab.class_names
+        # self.publish_rate = 100
+        # self.class_colors = visualize.random_colors(len(CLASS_NAMES))
+
+    def set_coordinate_trasformation(self):
+        # maybe mistake, after check, delete following 3 line.
+        self.tvec = np.array([0.259659, 0.221303, 0.854517], dtype=np.float32)
+        self.rvec = np.array([
+            [0.01391082878792294, 0.9998143798348266, 0.01333021822529676],
+            [0.9998941380955942, -0.0138525840224244, -0.004451799408139552],
+            [-0.00426631509639492, 0.01339073528237409, -0.9999012385051315]
+        ], dtype=np.float32)
+        self.marker_origin = np.array([0.24, -0.58, -0.100], dtype=np.float32)
+
+        # Marker position and pose from camera coordinate
+        tvec_from_camera = np.array([-0.221303, -0.259659, 0.854517], dtype=np.float32)
+        rvec_from_camera = np.array([
+            [0.01391082878792294, 0.9998143798348266, 0.01333021822529676],
+            [0.9998941380955942, -0.0138525840224244, -0.004451799408139552],
+            [-0.00426631509639492, 0.01339073528237409, -0.9999012385051315]
+        ], dtype=np.float32)
+
+        # Camera position and pose from marker coordinate
+        tvec_from_marker = np.dot(rvec_from_camera, tvec_from_camera) * -1
+        rvec_from_marker = rvec_from_camera.T
+
+        # Homogeneous coordinate transformation from camera to marker
+        marker_camera = np.array([
+            np.append(rvec_from_marker[0], tvec_from_marker[0]),
+            np.append(rvec_from_marker[1], tvec_from_marker[1]),
+            np.append(rvec_from_marker[2], tvec_from_marker[2]),
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+
+        # Homogeneous coordinate transformation from marker to robot
+        robot_marker = np.array([
+            [1.0, 0.0, 0.0, 0.24],
+            [0.0, 1.0, 0.0, -0.58],
+            [0.0, 0.0, 1.0, -0.1],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+
+        self.robot_camera = np.dot(robot_marker, marker_camera)
+
 
     def run(self):
-        self._result_pub = rospy.Publisher('~result', MaskRCNNMsg, queue_size=1)
-        vis_pub = rospy.Publisher('~visualization', Image, queue_size=1)
-        sub = rospy.Subscriber("/camera/color/image_raw", Image, self._image_callback, queue_size=1)
-        # sub = rospy.Subscriber('~input', Image, self._image_callback, queue_size=1)
-
-        rate = rospy.Rate(self._publish_rate)
-        while not rospy.is_shutdown():
-            if self._msg_lock.acquire(False):
-                msg = self._last_msg
-                self._last_msg = None
-                self._msg_lock.release()
-            else:
-                rate.sleep()
-                continue
-
-            if msg is not None:
-                np_image = self._cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
-
-                # Run detection
-                results = self._model.detect([np_image], verbose=0)
-                result = results[0]
-                result_msg = self._build_result_msg(msg, result)
-                self._result_pub.publish(result_msg)
-
-                # Visualize results
-                if self._visualization:
-                    vis_image = self._visualize(result, np_image)
-                    cv_result = np.zeros(shape=vis_image.shape, dtype=np.uint8)
-                    cv2.convertScaleAbs(vis_image, cv_result)
-                    image_msg = self._cv_bridge.cv2_to_imgmsg(cv_result, 'bgr8')
-                    vis_pub.publish(image_msg)
-
-            rate.sleep()
-
-    def run2(self):
-        self._result_pub = rospy.Publisher('~result', MaskRCNNMsg, queue_size=1)
-        vis_pub = rospy.Publisher('~visualization', Image, queue_size=1)
-        sub = rospy.Subscriber("/camera/color/image_raw", Image, self._image_callback, queue_size=1)
-        
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-            np_image = cv2.imread(TEST_IMG)
-            
-            # Run detection
-            t1 = time.time()
-            results = self._model.detect([np_image], verbose=0)
-            t2 = time.time()
-            result = results[0]
-            # result_msg = self._build_result_msg(msg, result)
-            # self._result_pub.publish(result_msg)
-            
-            # Print detection time
-            detection_time = t2 - t1
-            print("Detection time: ", round(detection_time, 2), " s")
-            
-            # Visualize results
-            if self._visualization:
-                vis_image = self._visualize(result, np_image)
-                cv_result = np.zeros(shape=vis_image.shape, dtype=np.uint8)
-                cv2.convertScaleAbs(vis_image, cv_result)
-                image_msg = self._cv_bridge.cv2_to_imgmsg(cv_result, 'bgr8')
-                vis_pub.publish(image_msg)
-                print("Published detected image!")
-                
-            rate.sleep()
-
-    def run3(self):
-        self._result_pub = rospy.Publisher(rospy.get_name() + '/MaskRCNNMsg', MaskRCNNMsg, queue_size=1)
-        self.vis_pub = rospy.Publisher(rospy.get_name() + '/visualization', Image, queue_size=1, latch=True)
+        # Define publisher
+        self.result_pub = rospy.Publisher(rospy.get_name() + '/MaskRCNNMsg', MaskRCNNMsg, queue_size=1)
+        self.visualization_pub = rospy.Publisher(rospy.get_name() + '/visualization', Image, queue_size=1, latch=True)
         self.marker_pub = rospy.Publisher(rospy.get_name() + '/axes', MarkerArray, queue_size=1)
-        self.getCalibrationMatrix()
 
-        self.detect_srv = rospy.Service(rospy.get_name() + '/MaskRCNNSrv', MaskRCNNSrv, self._get_frame)
+        # Define service
+        self.result_srv = rospy.Service(rospy.get_name() + '/MaskRCNNSrv', MaskRCNNSrv, self.wait_frame)
         print("Ready to detect objects. Please service call", rospy.get_name() +  '/MaskRCNNSrv')
         rospy.spin()
 
-    def _get_frame(self, req):
+    def wait_frame(self, req):
         res = MaskRCNNSrvResponse()
 
         print("Waiting frame...")
@@ -159,48 +130,37 @@ class MaskRCNNNode(object):
         depth_msg = rospy.wait_for_message("/phoxi_camera/depth_map", Image)
         print("Acquired frame!")
 
-        self._detect_objects(image_msg, depth_msg)
+        self.detect_objects(image_msg, depth_msg)
         res.detectedMaskRCNN = self.result_msg
 
         return res
 
-    def _detect_objects(self, image_msg, depth_msg):
-        np_image = self._cv_bridge.imgmsg_to_cv2(image_msg, 'bgr8')
-        np_depth = self._cv_bridge.imgmsg_to_cv2(depth_msg, '32FC1')
+    def detect_objects(self, image_msg, depth_msg):
+        np_image = self.cv_bridge.imgmsg_to_cv2(image_msg, 'bgr8')
+        np_depth = self.cv_bridge.imgmsg_to_cv2(depth_msg, '32FC1')
         
-        # Run detection
+        # Run inference
         t1 = time.time()
-        results = self._model.detect([np_image], verbose=0)
+        results = self.model.detect([np_image], verbose=0)
         t2 = time.time()            
         # Print detection time
-        detection_time = t2 - t1
-        print("Detection time: ", round(detection_time, 2), " s")
+        inference_time = t2 - t1
+        print("Inference time: ", round(inference_time, 2), " s")
 
         result = results[0]
-        self.result_msg, self.axes_msg = self._build_result_msg(image_msg, result, np_image, np_depth)
-        self._result_pub.publish(self.result_msg)
+        self.result_msg, self.axes_msg = self.build_result_msg(image_msg, result, np_image, np_depth)
+        self.result_pub.publish(self.result_msg)
         self.marker_pub.publish(self.axes_msg)
         
-        # Visualize results
-        if self._visualization:
-            vis_image = self._visualize(result, np_image)
-            cv_result = np.zeros(shape=vis_image.shape, dtype=np.uint8)
-            cv2.convertScaleAbs(vis_image, cv_result)
-            image_msg = self._cv_bridge.cv2_to_imgmsg(cv_result, 'bgr8')
-            self.vis_pub.publish(image_msg)
-            print("Published detected image!")
+        # Visualize results        
+        vis_image = self.visualize(result, np_image)
+        cv_result = np.zeros(shape=vis_image.shape, dtype=np.uint8)
+        cv2.convertScaleAbs(vis_image, cv_result)
+        image_msg = self.cv_bridge.cv2_to_imgmsg(cv_result, 'bgr8')
+        self.visualization_pub.publish(image_msg)
+        print("Published detected image!")
 
-    def getCalibrationMatrix(self):
-        file_path = os.path.join(ROOT_DIR, "config/realsense_intrinsic.xml")
-        fs = cv2.FileStorage(file_path, cv2.FILE_STORAGE_READ)
-        self.camera_matrix = fs.getNode("camera_matrix").mat()
-        self.dist_coeffs = fs.getNode("distortion_coefficients").mat()
-        # self.tvec = np.array([-0.221303, -0.259659, 0.854517], dtype=np.float32)
-        self.tvec = np.array([0.259659, 0.221303, 0.854517], dtype=np.float32)
-        self.rvec = np.array([[0.01391082878792294, 0.9998143798348266, 0.01333021822529676],[0.9998941380955942, -0.0138525840224244, -0.004451799408139552], [-0.00426631509639492, 0.01339073528237409, -0.9999012385051315]], dtype=np.float32)
-        self.marker_origin = np.array([0.24, -0.58, -0.100], dtype=np.float32)
-
-    def _build_result_msg(self, msg, result, image, depth):
+    def build_result_msg(self, msg, result, image, depth):
         result_msg = MaskRCNNMsg()
         result_msg.header = msg.header
         result_msg.header.frame_id = "base_link"
@@ -219,7 +179,7 @@ class MaskRCNNNode(object):
             class_id = result['class_ids'][i]
             result_msg.class_ids.append(class_id)
 
-            class_name = self._class_names[class_id]
+            class_name = self.class_names[class_id]
             result_msg.class_names.append(class_name)
 
             score = result['scores'][i]
@@ -254,7 +214,10 @@ class MaskRCNNNode(object):
                 print("The Center of Object (Camera Coordinate):", xyz_center_camera)
 
                 xyz_center_world = self.marker_origin + self.tvec + np.dot(self.rvec, xyz_center_camera)
+                xyz_center_camera_tmp = np.array([x_camera, y_camera, z_camera, 1.0], dtype=np.float32)
+                tmp = np.dot(self.robot_camera, xyz_center_camera_tmp)
                 print("The Center of Object (World Coordinate):", xyz_center_world)
+                print("The Center of Object (World Coordinate) tmp:", tmp)
 
                 center = Point()
                 center.x = xyz_center_world[0]
@@ -317,37 +280,18 @@ class MaskRCNNNode(object):
                 # y_axis_z = center.z
                 # result_msg.x_axis.append(x_axis)
                 # result_msg.y_axis.append(y_axis)
-
-            # mask = Image()
-            # mask.header = msg.header
-            # mask.height = result['masks'].shape[0]
-            # mask.width = result['masks'].shape[1]
-            # mask.encoding = "mono8"
-            # mask.is_bigendian = False
-            # mask.step = mask.width
-            # mask.data = (result['masks'][:, :, i] * 255).tobytes()
-            # result_msg.masks.append(mask)
+            
             result_msg.ids.append(i)
             result_msg.count += 1
         return result_msg, axes_msg
 
-    def _visualize(self, result, image):
-        # from matplotlib.backends.backend_agg import FigureCanvasAgg
-        # from matplotlib.figure import Figure
+    def estimate_object_pose(self, mask):
+        print("test")
 
-        # fig = Figure()
-        # canvas = FigureCanvasAgg(fig)
-        # axes = fig.gca()
-        # visualize.display_instances(image, result['rois'], result['masks'],
-        #                             result['class_ids'], CLASS_NAMES,
-        #                             result['scores'], ax = axes)
-        # fig.tight_layout()
-        # canvas.draw()
-        # result = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
+    def build_marker_msg(self):
+        print("test")
 
-        #_, _, w, h = fig.bbox.bounds
-        #result = result.reshape((int(h), int(w), 3))
-
+    def visualize(self, result, image):
         rois = result['rois']
         masks = result['masks']
         class_ids = result['class_ids']
@@ -358,7 +302,7 @@ class MaskRCNNNode(object):
             y1, x1, y2, x2 = rois[i]
             class_id = class_ids[i]
             score = scores[i]
-            label = CLASS_NAMES[class_id]
+            label = self.class_names[class_id]
             caption = "{} {:.3f}".format(label, score) if score else label
             cv2.putText(result, caption, (x1, y1 + 8), cv2.FONT_HERSHEY_TRIPLEX, 0.6, (0,255,0), 1, 8)
  
@@ -413,18 +357,11 @@ class MaskRCNNNode(object):
         
         return angle
 
-    def _image_callback(self, msg):
-        rospy.logdebug("Get an image")
-        if self._msg_lock.acquire(False):
-            self._last_msg = msg
-            self._msg_lock.release()
-
 def main():
     rospy.init_node('mask_rcnn')
 
     node = MaskRCNNNode()
-    # node.run()
-    node.run3()
+    node.run()
 
 if __name__ == '__main__':
     main()
